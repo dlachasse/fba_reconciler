db71f5e5bfcbf2d092b800636d7dd5c7bbee2a28
David La Chasse <david.lachasse@gmail.com>
2013-12-19 10:44:27 -0800

    Remove db sql comment

--- lib/fba_reconcile/database.rb
+++ lib/fba_reconcile/database.rb
@@ -15,13 +15,3 @@ class Database
 	end
 
 end
-
-		# sql = "SELECT inv.LocalSKU, sup.Cost, inv.Text1, inv.Price
-		# FROM [SE Data].[dbo].[Inventory] AS inv
-		# INNER JOIN [SE Data].[dbo].[InventorySuppliers] AS sup
-		# ON inv.LocalSKU = sup.LocalSKU
-		# 	WHERE inv.Category = 'Licensed Shirts'
-		# 	AND inv.LocalSKU NOT LIKE 'RJ%'
-		# 	And inv.QOH > 0
-		# 	AND (sup.Cost IS NOT NULL OR sup.Cost <> 0)"
-		# split @params[:client].execute sql
