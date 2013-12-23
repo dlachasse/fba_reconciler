9ab8a329e0e05ab85c6b17c0f9f38dc471b1728d
David La Chasse <david.lachasse@gmail.com>
2013-12-23 10:46:32 -0800

    Encapsulate class methods in self

--- lib/fba_reconcile/database.rb
+++ lib/fba_reconcile/database.rb
@@ -1,31 +1,33 @@
 class Database
 
-	def self.connect
-		client = TinyTds::Client.new(
-			:host => CFG["server"]["host"], 
-			:port => CFG["server"]["port"], 
-			:username => CFG["server"]["username"], 
-			:password => CFG["server"]["password"],
-			:timeout => CFG["server"]["timeout"],
-			:tds_version => CFG["server"]["tds_version"])
-	end
+	class << self
+		def connect
+			client = TinyTds::Client.new(
+				:host => CFG["server"]["host"], 
+				:port => CFG["server"]["port"], 
+				:username => CFG["server"]["username"], 
+				:password => CFG["server"]["password"],
+				:timeout => CFG["server"]["timeout"],
+				:tds_version => CFG["server"]["tds_version"])
+		end
 
-	def self.query
-		@client = connect
-		sql = "SELECT LocalSKU, Text1 AS FBA, QOH
-		FROM [SE Data].[dbo].[Inventory]
-			WHERE Category = 'Licensed Shirts'"
-		@client.execute sql
-	end
+		def query
+			@client = connect
+			sql = "SELECT LocalSKU, Text1 AS FBA, QOH
+			FROM [SE Data].[dbo].[Inventory]
+				WHERE Category = 'Licensed Shirts'"
+			@client.execute sql
+		end
 
-	def self.update(sku, fba)
-		@client = connect
-		sql = "UPDATE [SE Data].[dbo].[Inventory]
-			SET Text1 = #{fba}
-			WHERE LocalSKU = #{sku}"
-		result = @client.execute sql
-		result.do
-		puts "UPDATED :: #{sku} to #{fba}"
+		def update(sku, fba)
+			@client = connect
+			sql = "UPDATE [SE Data].[dbo].[Inventory]
+				SET Text1 = #{fba}
+				WHERE LocalSKU = #{sku}"
+			result = @client.execute sql
+			result.do
+			puts "UPDATED :: #{sku} to #{fba}"
+		end
 	end
 
 end
