25bccfb716b0be0d7f9aff744ba9b48f14262f5e
David La Chasse <david.lachasse@gmail.com>
2014-01-28 10:51:01 -0800

    Add recommendation report request + output

--- lib/fba_reconcile.rb
+++ lib/fba_reconcile.rb
@@ -14,6 +14,15 @@ CFG = YAML::load_file(File.join(File.expand_path("./"), "config.yml"))
 
 module FBAReconcile
 
+	def self.recs
+		%w(Inventory Selection Pricing Fulfillment ListingQuality).each do |cat|
+			recs = Request.new(:connect, variable: "@recommendation_category", value: cat)
+			data = recs.request_recommendations
+			f = File.new("./recommendations_#{cat.downcase}.txt", "w+")
+			f.puts data.to_xml(root: 'ListRecommendationsResult')
+		end
+	end
+
 	def self.start
 		FBA.new unless FBA.recent_report_downloaded?
 		Processor.new
