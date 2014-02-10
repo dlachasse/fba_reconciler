3dabc4a13aab1b8d7cf7472d938f79c96a5c1613
David La Chasse <david.lachasse@gmail.com>
2014-02-10 15:08:14 -0800

    Pass in market as option
    Raise error when market value has not been set. Also, don't print
    connect info, this isn't necessary.

--- lib/fba_reconcile/api.rb
+++ lib/fba_reconcile/api.rb
@@ -7,28 +7,35 @@ class Request
 
 	def initialize(method,opts={})
 		@opts = opts
+		@market = @opts[:market] if @opts[:market]
 		send(method)
 	end
 
-	def connect(market="hive")
-		puts "Connecting to MWS"
+	def connect
 		@@mws = MWS.new(
-		  host: CFG["#{market}"]["host"],
-		  aws_access_key_id: CFG["#{market}"]["aws_access_key_id"],
-		  aws_secret_access_key: CFG["#{market}"]["aws_secret_access_key"],
-		  seller_id: CFG["#{market}"]["seller_id"]
+		  host: CFG[@market]["host"],
+		  aws_access_key_id: CFG[@market]["aws_access_key_id"],
+		  aws_secret_access_key: CFG[@market]["aws_secret_access_key"],
+		  seller_id: CFG[@market]["seller_id"]
 		)
+		raise MarketplaceNotFound if !@market
 	end
 
 	# Returns id of request for later use
 	def request_report
-		response = @@mws.reports.request_report("ReportType" => @opts[:report_type]).parsed_response
+		opts = set_opts
+		response = @@mws.reports.request_report(opts).parsed_response
 		@request_id = Tools.find(response, "ReportRequestId")
+	end
+
+	def set_opts
+		opts = Hash.new
+		opts.merge!("MarketplaceIdList.Id.1" => CFG[@market]["marketplace_id"]) unless CFG[@market].nil?
+		opts.merge!("ReportType" => @opts[:report_type]) unless @opts[:report_type].nil?
 	end
 
 	# Checks status of report with request id generated in previous request
 	def retrieve_report_list
-		p "MWS: #{@@mws}"
 		status = @@mws.reports.get_report_list.parsed_response
 		@report_list = Tools.find(status, "ReportInfo")
 	end
